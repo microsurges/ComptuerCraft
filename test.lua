8dacad874d8df3bf278bb851ef9ffdf5d38c1044
Microsurges01 <bookmansammy@gmail.com>
2022-12-08 20:20:06 -0500

    Added a function

--- test.lua
+++ test.lua
@@ -1,2 +1,20 @@
-print("Hello World");
-turtle.dig();
+function Test()
+    local length = 5;
+
+    turtle.refuel();
+    local turtlefuel = turtle.getFuelLevel();
+    if turtlefuel >= 0 then
+        print("Enough fuel for turtle");
+    else
+        print("Not enough fuel for turtle");
+    end
+
+    while length > 0 do
+        turtle.digDown();
+        turtle.down();
+        turtle.dig();
+        turtle.forward();
+    end
+end
+
+Test();
