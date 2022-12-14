af5edc6e047592eb742aca067286960731b8dff2
Microsurges01 <bookmansammy@gmail.com>
2022-12-14 16:23:05 -0500

    asdadasd

--- ok.lua
+++ ok.lua
@@ -1,21 +1,18 @@
 function getOrientation()
-    local location1 = vector.new(gps.locate(2, false))
-    if not turtle.forward() then
-        for j = 1, 6 do
-            if not turtle.forward() then
-                turtle.dig()
-            else 
-                break 
-            end
-        end
+    local location1 = vector.new(gps.locate())
+    while not turtle.forward() do
+        turtle.dig() 
     end
-    local location2 = vector.new(gps.locate(2, false))
+    local location2 = vector.new(gps.locate())
     heading = location2 - location1
+    headingX = location2.x - location1.x
+    headingZ = location2.z - location1.z
     -- -x = 1
     -- -z = 2
     -- +x = 3
     -- +z = 4
-    return ((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
+    return ((headingX + math.abs(headingX) * 2) + (headingZ + math.abs(headingZ) * 3))
 end
 
-
+print(getOrientation())
+gps.locate()
