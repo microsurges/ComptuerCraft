68bdd576c33c4094e9a04b8ce5728da07d37338d
Microsurges01 <bookmansammy@gmail.com>
2022-12-14 17:28:20 -0500

    gsdfdsafdsaf

--- ok.lua
+++ ok.lua
@@ -5,30 +5,20 @@ function getOrientation()
     end
     location2 = vector.new(gps.locate(2, false))
     heading = location2 - location1
-    t= ((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
-    direction = {}
-    if t % 2 == 0 then
-        if t > 2 then
-            direction.z = true
-        else
-            direction.z = false
-        end
-    else
-        if t > 1 then
-            direction.x = true
-        else
-            direction.x = false
-        end
-    end
-    return direction
+    t = ((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
+    direction = {
+        [1] = "W",
+        [2] = "N",
+        [3] = "E",
+        [4] = "S"
+    }
+
+    return direction[t]
     -- -x = 1
     -- -z = 2
     -- +x = 3
     -- +z = 4
 end
 
-local direction = getOrientation()
-
-print(direction.x)
-print(direction.z)
+print(getOrientation())
 gps.locate()
